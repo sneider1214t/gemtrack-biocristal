5fbee666b163522e82b258d8c7e63e248842d9ab
Santa-Hub <samu07051@gmail.com>
2025-06-18 00:07:00 -0500

    Reestructuración: frontend y backend organizados

--- src/components/Gemstones.jsx
+++
@@ -1,4 +0,0 @@
-function Gemstones() {
-  return <div className="p-8 text-white text-2xl">Gestión de Gemas</div>;
-}
-export default Gemstones;
--- src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-function ProtectedRoute({ children }) {
-  const isAuth = localStorage.getItem('rol'); // ← antes decía "auth"
-  return isAuth ? children : <Navigate to="/" />;
-}
-
-export default ProtectedRoute;
