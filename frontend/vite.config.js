448240cb5efbab0a111b7101834f4cc53728d86f
jero-arango <arangorendonjeronimo@gmail.com>
2025-06-19 17:26:33 -0500

    actualiza pagesss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/gemtrack-biocristal/', // <-- Aquí pones el nombre de tu repo
   plugins: [react()],
 });
