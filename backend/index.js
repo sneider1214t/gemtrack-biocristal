53d841ca8df83c5fbfa4f634fb31581d08e6ea16
dayhaaCode-25 <dayhana280207@gmail.com>
2025-06-25 17:36:52 -0500

    Modificar APIs e iniciar test

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const db = require('./config/db');
-const app = express();
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import db from "./src/config/db.js"; // Asegúrate que el archivo tenga extensión .js
+import routerCategorias from "./src/routes/Categorias.routes.js";
+
+app = express();
 
 app.use(cors());
 app.use(express.json());
 
 // Ruta de prueba
-app.get('/', (req, res) => {
-  res.send('Servidor backend de Biocristal funcionando correctamente ✅');
+app.get("/", (req, res) => {
+  res.send("Servidor backend de Biocristal funcionando correctamente ✅");
 });
+
+app.use("/api/categorias", routerCategorias);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+export default app;
